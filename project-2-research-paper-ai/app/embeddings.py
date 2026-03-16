119118a70a93de2837ed626e4b1769c5e9d21fde
ARYAKRI <157015076+ARYAKRI@users.noreply.github.com>
2026-03-16 23:33:45 +0530

    Add function to create vector store from documents

--- project-2-research-paper-ai/app/embeddings.py
+++ project-2-research-paper-ai/app/embeddings.py
@@ -1,1 +1,21 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
 
+def create_vector_store(documents):
+
+    # Split documents into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100
+    )
+
+    texts = text_splitter.split_documents(documents)
+
+    # Convert text into embeddings
+    embeddings = OpenAIEmbeddings()
+
+    # Create vector database
+    vectorstore = FAISS.from_documents(texts, embeddings)
+
+    return vectorstore
